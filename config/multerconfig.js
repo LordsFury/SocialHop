cafced767f006514171e6e5a52b1b5986118be11
Abdullah <iabdullahkhan100.com>
2025-07-23 12:34:58 +0500

    all issues resolved

--- config/multerconfig.js
+++ config/multerconfig.js
@@ -1,38 +1,18 @@
-<<<<<<< HEAD
-const multer=require('multer');
-const path=require('path');
-const crypto=require('crypto');
+const multer = require('multer');
+const path = require('path');
+const crypto = require('crypto');
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './public/images/uploads')
-    },
-    filename: function (req, file, cb) {
-        crypto.randomBytes(12,function(err, name){
-            const fn=name.toString("hex")+path.extname(file.originalname);
-            cb(null, fn)
-        })
-    }
-  })
-  
-  const upload = multer({ storage: storage })
-=======
-const multer=require('multer');
-const path=require('path');
-const crypto=require('crypto');
+  destination: function (req, file, cb) {
+    cb(null, './public/images/uploads')
+  },
+  filename: function (req, file, cb) {
+    crypto.randomBytes(12, function (err, name) {
+      const fn = name.toString("hex") + path.extname(file.originalname);
+      cb(null, fn)
+    })
+  }
+})
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './public/images/uploads')
-    },
-    filename: function (req, file, cb) {
-        crypto.randomBytes(12,function(err, name){
-            const fn=name.toString("hex")+path.extname(file.originalname);
-            cb(null, fn)
-        })
-    }
-  })
-  
-  const upload = multer({ storage: storage })
->>>>>>> 85bcc1d (changes for deployment)
-  module.exports=upload;
+const upload = multer({ storage: storage })
+module.exports = upload;
